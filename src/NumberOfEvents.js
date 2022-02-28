52743d4abd6f5b540fe78b28d3dd634e0b46498b
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-02-28 16:03:58 -0500

    this.state -> this.props

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,27 +2,10 @@ import React, { Component } from 'react';
 import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-    state = { 
-        listLength: 32,
-        errorText: ''
-    }
-    
-    // handleListInput = (e) => {
-    //     const value = e.target.value;
-    //     if(value < 1 || value > 32) {
-    //         this.setState({
-    //             listLength: '',
-    //             errorText: 'Please enter a number from 1 to 32.'
-    //         })
-    //     } else {
-    //         this.setState({
-    //             listLength: value,
-    //             errorText: ''
-    //         })
-    //     }
-    //     this.props.update(NumberOfEvents(e.target.value));
-    // };
-
+    // state = { 
+    //     listLength: 32,
+    //     errorText: ''
+    // }
 
     render() { 
         return (
@@ -32,10 +15,9 @@ class NumberOfEvents extends Component {
                     type='number'
                     className='event-number-input'
                     value={this.props.listLength}
-                    // onChange={(e) => this.handleListInput(e)}
                     onChange={(e) => this.props.updateNumberOfEvents(e)}
                 />
-                <ErrorAlert text={this.state.errorText} />
+                <ErrorAlert text={this.props.errorText} />
             </div>
         );
     }
