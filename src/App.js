34a36506934d82c47b0807e178446e3598f26e0d
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-03-04 15:13:03 -0500

    import pie chart

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { WarningAlert } from './Alert';
 import {
   ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
 } from 'recharts';
+import { EventGenre } from './EventGenre';
 
 class App extends Component {
 
@@ -121,21 +122,24 @@ class App extends Component {
           errorText={this.state.errorText}
           handleListInput={this.handleListInput}
         />
-        <ResponsiveContainer
-          height={400}
-        >
-          <ScatterChart
-            margin={{
-              top: 20, right: 20, bottom: 20, left: 20,
-            }}
+        <div className='data-vis-wrapper'>
+          <EventGenre events={events} />
+          <ResponsiveContainer
+            height={400}
           >
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number fo events" allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
+            <ScatterChart
+              margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number fo events" allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList 
           events={this.state.events} 
         />
