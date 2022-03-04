79c9ebeef465fb2b1684f23f41c96fb75c37749e
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-03-04 13:41:50 -0500

    chart additions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
 import { WarningAlert } from './Alert';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 class App extends Component {
 
@@ -91,8 +94,18 @@ class App extends Component {
     this.mounted = false;
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render() {
-    
+
     const { warningMessage } = this.state;
 
     return (
@@ -108,6 +121,21 @@ class App extends Component {
           errorText={this.state.errorText}
           handleListInput={this.handleListInput}
         />
+        <ResponsiveContainer
+          height={400}
+        >
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" name="number fo events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList 
           events={this.state.events} 
         />
